48332d0e559f6a4b634a92270ff3447f62d1cd1e
shivnshshrma <shivnshshrma@gmail.com>
2026-05-27 13:49:06 +0530

    restructure the project and removed secrets

--- data/users.py
+++
@@ -1,8 +0,0 @@
-from ..models.User import User 
-
-user_db = {
-    "user1": User(username="user1", name="User1", email="[EMAIL_ADDRESS]", password="[PASSWORD]", phone="1234567890"),
-    "user2": User(username="user2", name="User2", email="[EMAIL_ADDRESS]", password="[PASSWORD]", phone="1234567890"),
-    "user3": User(username="user3", name="User3", email="[EMAIL_ADDRESS]", password="[PASSWORD]", phone="1234567890"),
-    
-}
